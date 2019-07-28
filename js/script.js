b96dfd630b214549d731f1b15ae87438f35bfcf8
guyo <guyo@localhost.localdomain>
2019-07-28 16:45:32 +0300

    added jquery

--- js/script.js
+++ js/script.js
@@ -73,35 +73,35 @@ function ShippingAddress(cityName, cityAvenueName, cityStreetName, nameOfBuildin
  
   
 
-//   $("form#customized-pizza").submit(function(event) {
-//     event.preventDefault();
-//     var customSize = $("select#size option:selected").val();
-//     var customcrust = $("select#crust option:selected").val();
-//     var customtopping = $("select#topping option:selected").val();
-//     var newPizzaOrder = new Placeorder(customSize , customcrust , customtopping);
+  $("form#customized-pizza").submit(function(event) {
+    event.preventDefault();
+    var customSize = $("select#size option:selected").val();
+    var customcrust = $("select#crust option:selected").val();
+    var customtopping = $("select#topping option:selected").val();
+    var newPizzaOrder = new Placeorder(customSize , customcrust , customtopping);
     
-//     // var totalOrderPrice = customSize + customcrust + customtopping;
+    // var totalOrderPrice = customSize + customcrust + customtopping;
 
-//     newPizzaOrder.costOfPizza();
+    newPizzaOrder.costOfPizza();
 
-//     totalOrderPrice.push(newPizzaOrder.price);
+    totalOrderPrice.push(newPizzaOrder.price);
 
 
 
-//     $("#pizza-size").show();
-//     $("#pizza-crust").show();
-//     $("#pizza-topping").show();
-//     $("#pizza-size").append("\t" + "\t" + customSize);
-//     $("#pizza-crust").append("\t" + "\t" + customcrust);
-//     $("#pizza-topping").append("\t" + "\t" + customtopping);
-//     $("#final-cost").text("\t" + "\t" + newPizzaOrder.totalCost());
+    $("#pizza-size").show();
+    $("#pizza-crust").show();
+    $("#pizza-topping").show();
+    $("#pizza-size").append("\t" + "\t" + customSize);
+    $("#pizza-crust").append("\t" + "\t" + customcrust);
+    $("#pizza-topping").append("\t" + "\t" + customtopping);
+    $("#final-cost").text("\t" + "\t" + newPizzaOrder.totalCost());
     
-//     var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $("#pizza-size").text() + " - " + customSize + '</td><td id="crust">' + $("#pizza-crust").text() + " - " + customcrust + '</td><td id="topping">' + $("#pizza-topping").text() + " - " + customtopping + '</td><td id="final-cost">' + $("#final-cost").text()+" - "+ newPizzaOrder.totalCost() + '</td></tr>'
+    // var newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' + $("#pizza-size").text() + " - " + customSize + '</td><td id="crust">' + $("#pizza-crust").text() + " - " + customcrust + '</td><td id="topping">' + $("#pizza-topping").text() + " - " + customtopping + '</td><td id="final-cost">' + $("#final-cost").text()+" - "+ newPizzaOrder.totalCost() + '</td></tr>'
 
-//       $("#pizza").append(newRow);
-//   });
-//   $("#checkout-btn").click(function() {
-//     location.reload();
-//   });
-//   });
+    //   $("#pizza").append(newRow);
+  });
+  $("#checkout-btn").click(function() {
+    location.reload();
+  });
+  });
 
